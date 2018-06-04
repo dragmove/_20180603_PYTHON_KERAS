5d392a7665328dd3416fa7c41a827b34c68808b4
dragmove <dragmove@naver.com>
2018-06-05 00:08:20 +0900

    feat: Working ann

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
-import keras
 import numpy
+import keras
+from keras import datasets
+from keras import layers, models
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
 
 def init():
     predict_by_dense_layer()
@@ -18,6 +22,99 @@ def predict_by_dense_layer():
     print('Targets :', y[2:])
     print('Predictions :', model.predict(x[2:]).flatten())
 
+# ANN
+# 분산 방식 모델링을 포함하는 함수형 구현
+def ANN_models_func(Nin, Nh, Nout):
+    x = layers.Input(shape=(Nin,))
+    h = layers.Activation('relu')(layers.Dense(Nout)(x))
+    y = layers.Activation('softmax')(layers.Dense(Nout)(h))
+
+    model = models.Model(x, y)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 연쇄 방식 모델링을 포함하는 함수형 구현
+def ANN_seq_func(Nin, Nh, Nout):
+    model = models.Sequential()
+    model.add(layers.Dense(Nh, activation='relu', input_shape=(Nin,)))
+    model.add(layers.Dense(Nout, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    return model
+
+# 분산 방식 모델링을 포함하는 객체지향형 구현
+class ANN_models_class(models.Model):
+    def __init__(self, Nin, Nh, Nout):
+        hidden = layers.Dense(Nh)
+        output = layers.Dense(Nout)
+        relu = layers.Activation('relu')
+        softmax = layers.Activation('softmax')
+
+        x = layers.Input(shape=(Nin,))
+        h = relu(hidden(x))
+        y = softmax(output(h))
+
+        super().__init__(x, y)
+        self.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 연쇄 방식 모델링을 포함하는 객체지향형 구현
+class ANN_seq_class(models.Sequential):
+    def __init__(self, Nin, Nh, Nout):
+        super().__init__()
+        self.add(layers.Dense(Nh, activation='relu', input_shape=(Nin,)))
+        self.add(layers.Dense(Nout, activation='softmax'))
+        self.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+def Data_func():
+    (X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
+
+    Y_train = np_utils.to_categorical(y_train)
+    Y_test = np_utils.to_categorical(y_test)
+
+    L, W, H = X_train.shape
+    X_train = X_train.reshape(-1, W * H)
+    X_test = X_test.reshape(-1, W & H)
+
+    X_train = X_train / 255.0
+    X_test = X_test / 255.0
+
+    return (X_train, Y_train), (X_test, Y_test)
+
+def plot_loss(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc=0)
+
+def plot_acc(history):
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc=0)
+
+def ann():
+    Nin = 784
+    Nh = 100
+    number_of_class = 10
+    Nout = number_of_class
+
+    model = ANN_seq_class(Nin, Nh, Nout)
+    (X_train, Y_train), (X_test, Y_test) = Data_func()
+
+    # training
+    history = model.fit(X_train, Y_train, epochs=1, batch_size=100, validation_split=0.2)
+    performance_test = model.evaluate(X_test, Y_test, batch_size=100) # TODO: Why occur error ?
+    print('Test Loss And Accuracy -> {:.2f}, {:.2f}'.format(*performance_test))
+
+    plot_loss(history)
+    plt.show()
+    plot_acc(history)
+    plt.show()
+
 # implementation
 if __name__ == '__main__':
-    init()
+    # init()
+    ann()
